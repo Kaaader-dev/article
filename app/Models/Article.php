a16d0abb1ad3cced2390a2f41106cd85456716e9
Ribes Alexandre <ribes.alexandre@gmail.com>
2021-10-04 16:57:06 +0200

    Commentaires

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,32 +1,53 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+/**
+ * Class Article
+ * @package App\Models
+ */
 class Article extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'description', 'content', 'user_id'];
 
+    /**
+     * Relation avec la table users
+     */
     public function user()
     {
         $this->belongsTo(User::class, 'user_id', 'id');
     }
 
+    /**
+     * Pivot avec la table articles_categories
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'articles_categories', 'article_id', 'id');
     }
 
+    /**
+     * Modification de l'attribut title avant insertion en base de donnée
+     *
+     * @param $value
+     */
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = ucfirst($value);
     }
 
+    /**
+     * Modification de l'attribut slug avant insertion
+     *
+     * @param $value
+     */
     public function setSlugAttribute($value)
     {
         $this->attributes['slug'] = Str::slug($value);
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,11 +1,29 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Category
+ * @package App\Models
+ */
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    //  On utilise pas les champs created_at et updated_at de laravel
+    public $timestamps = false;
+
+    /**
+     * Articles associés à la catégorie
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'articles_categories', 'category_id');
+    }
 }
